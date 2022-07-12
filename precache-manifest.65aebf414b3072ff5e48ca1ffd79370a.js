3abe97e36aa94206b547827008aa38fd42c1a462
Ulmie <ulmie1257@gmail.com>
2022-07-12 19:42:42 +0300

    Updates

---
+++ precache-manifest.65aebf414b3072ff5e48ca1ffd79370a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44421e97e41ac09991f5424d89d59787",
+    "url": "/react-website-v1/index.html"
+  },
+  {
+    "revision": "393b55fa88eee2e67a58",
+    "url": "/react-website-v1/static/css/main.47f00606.chunk.css"
+  },
+  {
+    "revision": "9218438ff743b96dcee2",
+    "url": "/react-website-v1/static/js/2.4ecea524.chunk.js"
+  },
+  {
+    "revision": "9d84f30ba44fdf9a7a4686c213bb5356",
+    "url": "/react-website-v1/static/js/2.4ecea524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393b55fa88eee2e67a58",
+    "url": "/react-website-v1/static/js/main.c5346fbc.chunk.js"
+  },
+  {
+    "revision": "5560325786c084172091",
+    "url": "/react-website-v1/static/js/runtime-main.1b8d7b28.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/react-website-v1/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/react-website-v1/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/react-website-v1/static/media/img-8.d211c298.jpg"
+  }
+]);
